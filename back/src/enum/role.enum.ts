7ce6a068674af03bf88040d456821aa140c53b09
DouglasDiaz <161756607+DouglasDiazR@users.noreply.github.com>
2024-07-25 13:49:29 -0500

    Roles de Usuarios

---
+++ back/src/enum/role.enum.ts
@@ -0,0 +1,8 @@
+export enum Role {
+  invited = 'invited',
+  registered = 'registered',
+  monthlyMembership = 'monthlyMembership',
+  annualMembership = 'annualMembership',
+  creator = 'creator',
+  admin = 'admin',
+}
