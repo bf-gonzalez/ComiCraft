055f00929e927bfe3be10d0fcedf76d58f0e1e62
BlskMax <alejandro8g8@gmail.com>
2024-08-02 12:05:07 -0600

    boton de google funcionando parcialmente

--- front/src/helpers/validateRegister.tsx
+++ front/src/helpers/validateRegister.tsx
@@ -50,3 +50,49 @@ export const validateRegister = (values: { [key: string]: string }) => {
 
   return errors;
 };
+
+export const validateCompleteProfile = (values: { [key: string]: string }) => {
+    const errors: { [key: string]: string } = {};
+  
+
+  
+  
+    if (!values.password) {
+        errors.password = "La contraseña es requerida";
+    } else if (values.password.length < 6) {
+        errors.password = "La contraseña debe tener al menos 6 caracteres";
+    }
+  
+    if (values.password !== values.confirmPassword) {
+        errors.confirmPassword = "Las contraseñas deben coincidir";
+    }
+  
+    if (!/^\d{10,}$/.test(values.phone)) {
+        errors.phone = "El teléfono debe tener al menos 10 dígitos";
+    }
+  
+    if (!values.address) {
+        errors.address = "La dirección es requerida";
+    }
+  
+    if (!values.dob) {
+        errors.dob = "La fecha de nacimiento es requerida";
+    } else {
+        const today = new Date();
+        const dob = new Date(values.birthdate);
+        if (dob > today) {
+            errors.birthdate = "La fecha de nacimiento no puede ser futura";
+        } else {
+            let age = today.getFullYear() - dob.getFullYear();
+            const monthDifference = today.getMonth() - dob.getMonth();
+            if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < dob.getDate())) {
+                age--;
+            }
+            if (age < 18) {
+                errors.birthdate = "Debes tener al menos 18 años";
+            }
+        }
+    }
+  
+    return errors;
+  };
--- front/src/utils/decodeGJWT.ts
+++ front/src/utils/decodeGJWT.ts
@@ -8,3 +8,18 @@ export default function decodeJwt(token: string) {
     const payload = JSON.parse(atob(parts[1]));
     return { header, payload};
 }
+
+// export function decodeJwt2(token: string) {
+//     const base64Url = token.split('.')[1];
+//     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+//     const jsonPayload = decodeURIComponent(
+//         atob(base64)
+//             .split('')
+//             .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+//             .join('')
+//     );
+
+//     return JSON.parse(jsonPayload);
+// }
+
+
